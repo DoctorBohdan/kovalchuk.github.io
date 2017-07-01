19b730b86cf5d39e129c3c688a811f97d5f3bd87
Bohdan Kovalchuk <b.kovalchuk.photo@gmail.com>
2017-07-01 11:28:50 +0300

    Delete HTMLAcademy folder
    Delete HTMLAcademy folder

--- HTML Academy/HTML Academy Intensiv 2014/Barbershop/js/gallery.js
+++
@@ -1,5 +0,0 @@
-var gallery = document.querySelector('.gallery');
-var prev = document.querySelector('.gallery-prev');
-
-gallery.classList.add('gallery-live');
-prev.setAttribute('disabled', 'disabled');
